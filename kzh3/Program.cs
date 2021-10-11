f69970ec8dd26628a07f88d6a1a8ac356af17075
TorokDan <dani.torok8@gmail.com>
2021-10-11 19:01:37 +0200

    Nem tok ide mit írni.....

--- kzh3/Program.cs
+++ kzh3/Program.cs
@@ -10,6 +10,71 @@ namespace kzh3
     {
         static void Main(string[] args)
         {
+            double[] szamok = Create();
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.Write(szamok[i] + " ");
+            }
+            Console.WriteLine();
+            double[] randomSzamok = Create(10);
+            for (int i = 0; i < randomSzamok.Length; i++)
+            {
+                Console.Write(randomSzamok[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(Negative(szamok));
+            Rendez(ref szamok);
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine(szamok[i]);
+            }
+
+        }
+        static double[] Create()
+        {
+            double[] tomb = { -6.5, 5.2, -7.1, 4.2, 6.0, 8.3, -0.7, -4.8, -9.1, 10 };
+            return tomb;
+        }
+        static double[] Create(int n)
+        {
+            double[] tomb = new double[n];
+            Random rnd = new Random();
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = rnd.Next(-100, 100);
+            }
+            return tomb;
+        }
+        static bool Negative(double[] tomb)
+        {
+            double negativ = 0;
+            double pozitiv = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] < 0)
+                {
+                    pozitiv++;
+                }
+                if (tomb[i] > 0)
+                {
+                    negativ--;
+                }
+            }
+            return negativ > pozitiv;
+        }
+        static void Rendez(ref double[] T)
+        {
+            for (int i = 1; i < T.Length; i++)
+            {
+                int j = i - 1;
+                double seged = T[i];
+                while (j >= 0 && T[j] < seged)
+                {
+                    T[j + 1] = T[j];
+                    j--;
+                }
+                T[j + 1] = seged;
+            }
         }
     }
 }
